11b874c8d35e5ce080ebc09c63f908eb344f92f6
Miroslav Toykin <mt@truefuture.io>
2022-07-11 14:33:29 +0300

    added .env and move private key and rpc url there

--- deploy.js
+++ deploy.js
@@ -1,15 +1,13 @@
 const ethers = require("ethers");
 const fs = require("fs-extra");
+require("dotenv").config();
 
 async function main() {
     //http://127.0.0.1:7545
     // do things
 
-    const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:7545');
-    const wallet = new ethers.Wallet(
-        '816b012e776dca32e6c378e75a75cd4a33737dac6c88e081d7109a1818e43324',
-        provider
-    );
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY,provider);
 
     const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi', 'utf8');
     const binary = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.bin', 'utf8');
